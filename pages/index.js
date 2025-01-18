f5ac24297ac92f0e66942f8e933667e970261e8e
Aaron Lee <123liqixin@gmail.com>
2025-01-18 15:34:04 +0800

    11122

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import { useEffect } from 'react';
 export default function Home() {
   useEffect(() => {
     // 这里填写你的实际系统地址
-    //window.location.href = 'http://124.220.189.240:8080';
-    window.location.href = 'http://wisnet.online';
+    window.location.href = 'http://124.220.189.240:8080';
+    //window.location.href = 'http://wisnet.online';
   }, []);
 
   return <div>Loading...</div>;
